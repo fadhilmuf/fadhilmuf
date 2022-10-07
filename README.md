9b7647702cb6c841d8f6234b45d6a16c1ec64dbc
fadhilmuf <fadhilmuf.work@gmail.com>
2022-10-07 23:15:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://fadhilmuf.github.io/portfolio/
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#164e63', 
+        dark: '#1e293b',
+        ld: '#0077b5',
+        ig: '#E4405F',
+        gh: '#181717'
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
