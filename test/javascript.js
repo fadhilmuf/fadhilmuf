fd3b601ae3e7e1b11cbe1dfe741feb95a2eb169c
fadhilmuf <112771091+fadhilmuf@users.noreply.github.com>
2023-04-10 19:31:11 +0700

    bersih-bersih

--- test/javascript.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('question-form').addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent the form from refreshing the page
-  var question = document.getElementById('question').value;
-  var email = "mailto:fadhilmuf.work@gmail.com?subject=Question&body=" + question;
-  window.location.href = email;
-});
